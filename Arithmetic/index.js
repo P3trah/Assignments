d971855fa3897364b01326fd3ad05d194407c438
Petrah <+>
2024-01-13 22:57:27 +0300

    Assignment Done

---
+++ Arithmetic/index.js
@@ -0,0 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply)
+
+
+const random = Math.floor(Math.random() * 100) + 1; 
+const num3 = 14;
+const num4 = 10;
+console.log(random);
+
+const mod = num3 % num4;
+console.log (mod)
+
+
+const max = Math.max(10, 15, 20);
+console.log(max)
