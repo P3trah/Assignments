8f5fa22a074ebe2b38020821a596266a5807d635
Petrah <+>
2024-01-14 00:15:30 +0300

    Assignment Done

---
+++ ScopeLab/index.js
@@ -0,0 +1,23 @@
+var customerName ="bob"
+
+function upperCaseCustomerName() {
+    customerName = "customerName.toUpperCase()";
+    console.log (customerName)
+}
+function bestCustomer () {
+    bestCustomer ="not bob";
+    console.log (bestCustomer)
+}
+function overwriteBestCustomer(newbestcustomer) {
+    bestCustomer = "Maybe bob"
+    console.log (bestCustomer)
+}
+
+const leastFavoriteCustomer = "Betty"
+
+ function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer="newLeastFavoriteCustomer"
+ }
+
+
+
