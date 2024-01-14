6df41e2474c16a36b606a2696ed858a4037039b4
Petrah <+>
2024-01-14 13:57:24 +0300

    Assignment Done

---
+++ Looping/index.js
@@ -0,0 +1,16 @@
+function writeCards(names, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+}
+ 
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
